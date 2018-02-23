6afa8931a57b241557e986accda074ec48957e06
vitaly_gaiduchok_PC <vitaly_gaiduchok_PC@admins-mac-mini.localdomain>
2018-02-23 12:18:08 +0200

    Go to the Lambda 13

--- src/main/java/Servlet/HelloServlet.java
+++ src/main/java/Servlet/HelloServlet.java
@@ -83,7 +83,7 @@ public class HelloServlet extends HttpServlet {
             superReturn.addAll(returnAllFields(rd.Metadata, vars));
 
         }
-        resp.getWriter().append(superReturn.toString());
+        resp.getWriter().append(new Gson().toJson(superReturn));
 
 //        String disterString = "{![Account].AccountNumber + '  ' + [Account].Name} Account.Name Account.notName {![Account].JigsawCompanyId2} + "
 //                + "{![Account].Name1} + [Account.Name] + Account. AccountNumber + [Account].Name  {![Account].Name}sd {zxc} [Account]. namz + Account.names + "
